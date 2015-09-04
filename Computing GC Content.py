dfed06ccd81814a277e1a3304927cb8adc5880bf
DeborahPerez <perezdeboraha@gmail.com>
2015-09-04 16:43:19 -0400

    Completed Computing GC Content Problem by using a for loop and remembering the largest GC content value

--- Computing GC Content.py
+++ Computing GC Content.py
@@ -1,43 +1,32 @@
 
 # coding: utf-8
 
-# In[16]:
+# In[33]:
 
 DataSet = ">Rosalind_6404CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCCTCCCACTAATAATTCTGAGG>Rosalind_5959CCATCGGTAGCGCATCCTTAGTCCAATTAAGTCCCTATCCAGGCGCTCCGCCGAAGGTCTATATCCATTTGTCAGCAGACACGC>Rosalind_0808CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGACTGGGAACCTGCGGGCAGTAGGTGGAAT"
 delimiter = '>'
 RFLIST = DataSet.split(delimiter)
-
+#Created a list of all individual FASTA strands
 def delete_head(RFLIST):
     del RFLIST[0]
     
 delete_head(RFLIST)
-print RFLIST
 
-#def FASTAsection(DataSet):
-#   positions = []
-#    startposition = 0
+#Calculated GC content of each strand and had Python remember the largest and the FASTA ID of that strand as it went through the list
+def GC_CONTENT(RFLIST):
+    largest = None
+    for STRAND in RFLIST:
+        G = float(STRAND.count("G"))
+        C = float(STRAND.count("C"))
+        L = float(len(STRAND)-13)
+        GC_COUNT = float(((G + C) / L) * 100)
+        STRAND_ID = STRAND[0:14]
+    if largest is None or GC_COUNT > largest:
+        largest = GC_COUNT
+    print STRAND_ID
+    print round(largest, 6)
 
-#    while True:
-#       i = DataSet.find(">", startposition)
-#        if i == -1: break
-#        positions.append(i)
-#        startposition = i + 1
-
-#    return positions
-
-#FASTAsection(DataSet)
-
-#x = FASTAsection(DataSet)
-
-#for i in x:
-    #sequences = []
-    #s = (i + 1)
-    #print DataSet[i:s]
-    #sequences.append(seq)
-
-
-
-
+GC_CONTENT(RFLIST)
 
 
 # In[ ]:
