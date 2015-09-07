75a5afea3687f7169d9ac8539d02b9c7de70124f
DeborahPerez <perezdeboraha@gmail.com>
2015-09-06 22:52:40 -0400

    created lists out of nucleotides of each strand

---
+++ Counting Point Mutations.py
@@ -0,0 +1,24 @@
+
+# coding: utf-8
+
+# string1 = raw_input("Insert First String:")
+# string2 = raw_input("Insert Second String:")
+# 
+# print string1
+# print string2
+# 
+# 
+# ls1 = list(string1)
+# ls2 = list(string2)
+# 
+# print ls1
+# print ls2
+# 
+# 
+# 
+# 
+
+# In[ ]:
+
+
+
