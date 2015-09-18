bfdcb2f27df685c2249b784cd190b53f3f19bbe5
kstatebioinfo <kstate.bioinformatics@gmail.com>
2015-09-18 11:31:06 -0400

    Added ability to count lowercase nucleotides because both are used in FASTA files

--- CountingDNANucleotides.py
+++ CountingDNANucleotides.py
@@ -4,11 +4,16 @@
 # In[1]:
 
 DNA = raw_input ("Enter DNA Sequence:") 
-
+# count uppercase
 A = DNA.count("A")
 C = DNA.count("C")
 G = DNA.count("G")
 T = DNA.count("T")
+# count lowercase:
+A = A + DNA.count("a")
+C = C + DNA.count("c")
+G = G + DNA.count("g")
+T = T + DNA.count("t")
 
 print A, C, G, T
 
