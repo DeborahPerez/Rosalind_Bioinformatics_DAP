9e8dd945117ea5299a000957a7ba014f7a72a092
DeborahPerez <perezdeboraha@gmail.com>
2015-09-21 18:00:06 -0400

    Calculated probability for choosing combinations. Created lists with offspring possibilities due to certain combinations of k m and n individuals

--- MendelsFirstLaw.py
+++ MendelsFirstLaw.py
@@ -1,18 +1,73 @@
 
 # coding: utf-8
 
+# In[17]:
+
+#Need to create loop to count dominant offspring per combination and then calculate the probability.
+
+k = 2
+m = 2
+n = 2
+#population total
+total = float(k + m + n)
+
+#probability that both individuals are k
+prob2k = k/total + k/total
+#probability that both individuals are m
+prob2m = m/total + m/total
+#probability that both individuals are m
+prob2n = n/total + n/total
+
+#probability that individuals are k and m
+probkm = k/total + m/total
+#probability that individuals are k and n
+probkn = k/total + n/total
+#probability that individuals are m and n
+probmn = m/total + n/total
+
+#Alleles for individuals k, m, and n
+k_allele = ['A','A']
+m_allele = ['A','a']
+n_allele = ['a','a']
+
+#Probability that prob2k will produce offspring that inherit a dominant allele
+import itertools
+kk = k_allele, k_allele
+kk_off = list(itertools.product(*kk))
+print kk_off
+
+#Probability that prob2m will produce offspring that inherit a dominant allele
+import itertools
+mm = m_allele, m_allele
+mm_off = list(itertools.product(*mm))
+print mm_off
+
+#Probability that prob2k will produce offspring that inherit a dominant allele
+import itertools
+nn = n_allele, n_allele
+nn_off = list(itertools.product(*nn))
+print nn_off
+
+#Probability that k and m produce offspring that inherit a dominant allele
+import itertools
+km = k_allele, m_allele
+km_off = list(itertools.product(*km))
+print km_off
+
+#Probability that k and n produce offspring that inherit a dominant allele
+import itertools
+kn = k_allele, n_allele
+kn_off = list(itertools.product(*kn))
+print kn_off
+
+#Probability that m and n produce offspring that inherit a dominant allele
+import itertools
+mn = m_allele, n_allele
+mn_off = list(itertools.product(*mn))
+print mn_off
+
+
 # In[ ]:
 
-#Given: Three positive integers k, m, and n, representing a population containing  organisms: k individuals are homozygous dominant for a factor, m are heterozygous, and n are homozygous recessive.
 
-#Return: The probability that two randomly selected mating organisms will produce an individual possessing a dominant allele (and thus displaying the dominant phenotype). Assume that any two organisms can mate.
-
-#k = AA, m = Aa, n = aa, ie: K + m = AA * Aa = AA, AA, Aa, Aa ; K + n = AA * aa = Aa, Aa, Aa, Aa ; m + n = Aa * aa = Aa, Aa, aa, aa
-
-#probability for k and m that offspring has dominant allele = 2 + 2 + 1 + 1 = 6/8
-#probability for k and n that offspring has dominant allele = 1 + 1 + 1 + 1 = 4/8
-#probability for n and m that offspring has dominant allele = 1 + 1 + 0 + 0 = 2/8
-#probability for K and K that offspring has dominant allele = 1 = 8/8
-#probability for m and m that offspring has dominant allele = 2 + 1 + 1 + 0 = 4/8
-#probability for n and n that pffspring has dominant allele = 0 = 0/8
 
