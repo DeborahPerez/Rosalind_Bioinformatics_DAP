d45616d2b0485998428c9a8bdd07c9d613e916e8
DeborahPerez <perezdeboraha@gmail.com>
2015-09-23 16:16:16 -0400

    Created a loop to calculate the phenotypes for all possible combinations of offsprings

--- MendelsFirstLaw.py
+++ MendelsFirstLaw.py
@@ -1,9 +1,18 @@
 
 # coding: utf-8
 
-# In[17]:
+# In[19]:
 
-#Need to create loop to count dominant offspring per combination and then calculate the probability.
+#Description: 
+#Given: Three positive integers k, m, and n, representing a population containing k+m+n organisms: 
+#k individuals are homozygous dominant for a factor, m are heterozygous, and n are homozygous recessive.
+
+#Return: The probability that two randomly selected mating organisms will produce an individual possessing a dominant allele 
+#and thus displaying the dominant phenotype. Assume that any two organisms can mate.
+
+#Created By: Deborah Perez
+
+#Leftoff: Actually calculate the probability and almost done!
 
 k = 2
 m = 2
@@ -65,6 +74,35 @@ import itertools
 mn = m_allele, n_allele
 mn_off = list(itertools.product(*mn))
 print mn_off
+
+#Defining a function to determine dominant phenotypes by checking for the presence of dominant characteristics 
+def find_dominant_phenotype(x):
+    dominantphenotype = 0
+    for allele in x:
+        if 'A' in allele:
+            dominantphenotype += 1
+    print dominantphenotype
+    
+#Using find_dominant_phenotype function to count offspring with dominant phenotypes in k&k
+find_dominant_phenotype(kk_off)
+
+#Using find_dominant_phenotype function to count offspring with dominant phenotypes in m&m
+find_dominant_phenotype(mm_off)
+
+#Using find_dominant_phenotype function to count offspring with dominant phenotypes in n&n
+find_dominant_phenotype(nn_off)
+
+#Using find_dominant_phenotype function to count offspring with dominant phenotypes in k&m
+find_dominant_phenotype(km_off)
+
+#Using find_dominant_phenotype function to count offspring with dominant phenotypes in k&n
+find_dominant_phenotype(kn_off)
+
+#Using find_dominant_phenotype function to count offspring with dominant phenotypes in m&n
+find_dominant_phenotype(mn_off)
+
+        
+            
 
 
 # In[ ]:
