817bc2d026e8c209877ddb20f1e8418eb74891ae
DeborahPerez <perezdeboraha@gmail.com>
2015-10-19 22:42:19 -0400

    Created a list from the string of codons to easily translate using the dictionary in a for loop

--- TranslatingRNAintoProtein.py
+++ TranslatingRNAintoProtein.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[12]:
+# In[6]:
 
 #Description:
 #Given: An RNA string s corresponding to a strand of mRNA (of length at most 10 kbp).
@@ -14,15 +14,10 @@ SampleDataset = "AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA"
 
 #Dictionary to translate RNA to protein
 RNA_Codon_Translation = {"UUU" : "F", "UUC" : "F", "UUA" : "L", "UUG" : "L", "UCU" : "S", "UCC" : "S", "UCA" : "S", "UCG" : "S", "UAU" : "Y", "UAC" : "Y", "UAA" : "X", "UAG" : "X", "UGU" : "C", "UGC" : "C", "UGA" : "X", "UGG" : "W", "CUU" : "L", "CUC" : "L", "CUA" : "L", "CUG" : "L", "CCU" : "P", "CCC" : "P", "CCA" : "P", "CCG" : "P", "CAU" : "H", "CAC" : "H", "CAA" : "Q", "CAG" : "Q", "CGU" : "R", "CGC" : "R", "CGA" : "R", "CGG" : "R", "AUU" : "I", "AUC" : "I", "AUA" : "I", "AUG" : "M", "ACU" : "T", "ACC" : "T", "ACA" : "T", "ACG" : "T", "AAU" : "N", "AAC" : "N", "AAA" : "K", "AAG" : "K", "AGU" : "S", "AGC" : "S", "AGA" : "R", "AGG" : "R", "GUU" : "V", "GUC" : "V", "GUA" : "V", "GUG" : "V", "GCU" : "A", "GCC" : "A", "GCA" : "A", "GCG" : "A", "GAU" : "D", "GAC" : "D", "GAA" : "E", "GAG" : "E", "GGU" : "G", "GGC" : "G", "GGA" : "G", "GGG" : "G"}
-#RNA_Codon_Translation[SampleDataset]
 
-#import re
-#SDList = re.findall('...', SampleDataset)
-#set(SDList)
-
-#RNA_Codon_Translation[SDList]
-
-    
+#Created a list from the string parsing at every nth place, n=3
+SDList = [SampleDataset[i:i+3] for i in range(0, len(SampleDataset), 3)]
+print SDList  
 
 
 # In[ ]:
