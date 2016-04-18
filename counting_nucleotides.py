a281e8c7f34a986b63871a8fe9f4bbbf423bf6ee
Deborah Perez <Deborah.a.perez@gmail.com>
2016-04-18 12:35:03 -0400

    Added introduction and accounted for multiple lines in STDIN

---
+++ counting_nucleotides.py
@@ -0,0 +1,38 @@
+########################################################################
+#    USAGE:
+#       python3 counting_nucleotides.py
+#   DESCRIPTION:
+#       Summarize counts of all four DNA bases for a string
+#       entered through standard input
+#   ROSALIND PROBLEM:
+#       http://rosalind.info/problems/dna/
+#-----------------------------------------------------------------------
+#   CREATED BY: Deborah Perez
+#   VERSION:    20160418
+########################################################################
+
+#-STDIN-----------------------------------------------------------------
+import sys    # import "sys" to read from STDIN
+dna = sys.stdin.read().splitlines()    # read in the input from STDIN
+#-----------------------------------------------------------------------
+
+#-MAINCODE--------------------------------------------------------------
+def counting_nucleotides(dna):
+    a = 0
+    c = 0
+    g = 0
+    t = 0
+
+    for line in dna:
+        a += line.count("A")
+        c += line.count("C")
+        g += line.count("G")
+        t += line.count("T")
+
+    eachNucleotideCount = a, c, g , t
+    return eachNucleotideCount
+#-MAINCODE--------------------------------------------------------------
+
+#-STDOUT----------------------------------------------------------------
+print (' '.join(map(str, (counting_nucleotides(dna)))))
+#-STDOUT----------------------------------------------------------------
