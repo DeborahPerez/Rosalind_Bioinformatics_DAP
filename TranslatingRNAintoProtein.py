ea93e8cf78c11a0f1274dea3316b2a3d6d35b59f
Deborah Perez <Deborah.a.perez@gmail.com>
2016-04-26 10:47:34 -0400

    turning this into a function and adding standard input and output code. Also adding comments and changing names

--- TranslatingRNAintoProtein.py
+++ TranslatingRNAintoProtein.py
@@ -1,40 +1,37 @@
+########################################################################
+#    USAGE:
+#       python3 TranslatingRNAintoProtein.py
+#   DESCRIPTION:
+#       Translate an RNA sequence into a Protein sequence
+#   ROSALIND PROBLEM:
+#       http://rosalind.info/problems/prot/
+#-----------------------------------------------------------------------
+#   CREATED BY: Deborah Perez
+#   VERSION:    20160426
+########################################################################
 
-# coding: utf-8
+import sys   
 
-# In[6]:
+# Dictionary to translate RNA to protein
+ rnaDictionary = {"UUU" : "F", "UUC" : "F", "UUA" : "L", "UUG" : "L", "UCU" : "S", "UCC" : "S", "UCA" : "S", "UCG" : "S", "UAU" : "Y", "UAC" : "Y", "UAA" : "X", "UAG" : "X", "UGU" : "C", "UGC" : "C", "UGA" : "X", "UGG" : "W", "CUU" : "L", "CUC" : "L", "CUA" : "L", "CUG" : "L", "CCU" : "P", "CCC" : "P", "CCA" : "P", "CCG" : "P", "CAU" : "H", "CAC" : "H", "CAA" : "Q", "CAG" : "Q", "CGU" : "R", "CGC" : "R", "CGA" : "R", "CGG" : "R", "AUU" : "I", "AUC" : "I", "AUA" : "I", "AUG" : "M", "ACU" : "T", "ACC" : "T", "ACA" : "T", "ACG" : "T", "AAU" : "N", "AAC" : "N", "AAA" : "K", "AAG" : "K", "AGU" : "S", "AGC" : "S", "AGA" : "R", "AGG" : "R", "GUU" : "V", "GUC" : "V", "GUA" : "V", "GUG" : "V", "GCU" : "A", "GCC" : "A", "GCA" : "A", "GCG" : "A", "GAU" : "D", "GAC" : "D", "GAA" : "E", "GAG" : "E", "GGU" : "G", "GGC" : "G", "GGA" : "G", "GGG" : "G"}
+    
+# Created a list from the string parsing at every nth place, n=3 and print
+def _translation(rna):
+    for strand in rna:
+        codonList = [strand[i:i+3] for i in range(0, len(strand), 3)]
 
-#Description:
-#Given: An RNA string s corresponding to a strand of mRNA (of length at most 10 kbp).
-#Return: The protein string encoded by s.
-
-#Created By: Deborah Perez
-
-#Sample Dataset in a string
-SampleDataset = "AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA"
-
-#Dictionary to translate RNA to protein
-RNA_Trans = {"UUU" : "F", "UUC" : "F", "UUA" : "L", "UUG" : "L", "UCU" : "S", "UCC" : "S", "UCA" : "S", "UCG" : "S", "UAU" : "Y", "UAC" : "Y", "UAA" : "X", "UAG" : "X", "UGU" : "C", "UGC" : "C", "UGA" : "X", "UGG" : "W", "CUU" : "L", "CUC" : "L", "CUA" : "L", "CUG" : "L", "CCU" : "P", "CCC" : "P", "CCA" : "P", "CCG" : "P", "CAU" : "H", "CAC" : "H", "CAA" : "Q", "CAG" : "Q", "CGU" : "R", "CGC" : "R", "CGA" : "R", "CGG" : "R", "AUU" : "I", "AUC" : "I", "AUA" : "I", "AUG" : "M", "ACU" : "T", "ACC" : "T", "ACA" : "T", "ACG" : "T", "AAU" : "N", "AAC" : "N", "AAA" : "K", "AAG" : "K", "AGU" : "S", "AGC" : "S", "AGA" : "R", "AGG" : "R", "GUU" : "V", "GUC" : "V", "GUA" : "V", "GUG" : "V", "GCU" : "A", "GCC" : "A", "GCA" : "A", "GCG" : "A", "GAU" : "D", "GAC" : "D", "GAA" : "E", "GAG" : "E", "GGU" : "G", "GGC" : "G", "GGA" : "G", "GGG" : "G"}
-
-#Created a list from the string parsing at every nth place, n=3 and print
-SDList = [SampleDataset[i:i+3] for i in range(0, len(SampleDataset), 3)]
-
-#loop through the list using the dictionary and appending the translation to a new string. If "X" is a translation the 
-#loop will stop while not translating "X" 
-PROTEIN = []
-for codon in SDList:
-    PRO = RNA_Trans[codon]
-    if PRO != "X":
-        PROTEIN.append(PRO)
+# loop through the list using the dictionary and appending the translation to a new string. If "X" is a translation the 
+# loop will stop while not translating "X" 
+protein = []
+for codon in CodonList:
+    aminoAcid = rnaToProteinDictionary[codon]
+    if aminoAcid != "X":
+        PROTEIN.append(aminoAcid)
         continue
-    else:
-#join converts the list into a string of characters by "join"ing the elements of the list into a string        
-        print ''.join(PROTEIN)
-
-
-
-
-
-# In[ ]:
-
-
-
+    else:     
+        return ''.join(protein)
+#-MAINCODE-------------------------------------------------------------- 
+rna = sys.stdin.read().splitlines()    # read in the input from STDIN
+#print ('\n'.join(map(str, (transcription(dna)))))
+print (reverseComplement(dna))
+#-----------------------------------------------------------------------
