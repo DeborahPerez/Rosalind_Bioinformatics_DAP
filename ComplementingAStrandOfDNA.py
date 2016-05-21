8170ed1766d7be7359c46d6741fa34bd52cb2c07
Deborah Perez <Deborah.a.perez@gmail.com>
2016-05-21 15:05:42 -0400

    Added comments to increase readability

--- ComplementingAStrandOfDNA.py
+++ ComplementingAStrandOfDNA.py
@@ -7,38 +7,51 @@
 #       http://rosalind.info/problems/revc/
 #-----------------------------------------------------------------------
 #   CREATED BY: Deborah Perez
-#   VERSION:    20160429
+#   VERSION:    20160521
 ########################################################################
-
 import sys 
-
 # ---reverse_complement-------------------------------------------------
 # Converts a strand of dna to its reverse complement
+# @param dna one or more lines of dna text
+# @return string of nucleotides as reverse complement
+# ----------------------------------------------------------------------
 def reverse_complement(dna):
-    dnaComplement = ''    # Create empty string as output
-# 
+# Initializes string to be returned for reverse complement
+    dnaComplement = ''
+# Reverses the order of the input string
     for line in dna:
         reverseDna = _reverse(line) 
+# Applies complements of reversed input to dnaComplement
         for nucleotide in reverseDna:
             dnaComplement += _complement(nucleotide)
 # Returns output           
-    return dnaComplement    
+    return dnaComplement
+# ----------------------------------------------------------------------
 # ---_reverse-----------------------------------------------------------
 # Reverses text
-# 4. Iterate through each character in the text
-# 5. Append the index correlated to the value of count
-def _reverse(text):    # Set text as variable for function
-    reversedText = ''    # Create empty string as output
-    count = len(text) - 1    # Begin countdown from length of text - 1
-    for character in text:    # 4
-        reversedText += text[count]    # 5
-        count -=1    # Reduce count by 1
-    return reversedText    # return output
+# @param string of text
+# @return string text in reverse order
 # ----------------------------------------------------------------------
-# Translates a dna nucleotide to its dna complement nucleotide
-# 6. Set nucleotide as variable for function
+def _reverse(text):
+# Initializes string to be returned for reverse order
+    reversedText = ''
+# Appends the index of text correlated to the value of count
+    count = len(text) - 1
+    for character in text:
+        reversedText += text[count]
+        count -=1
+# Returns output      
+    return reversedText
+# ----------------------------------------------------------------------
+# ---_complement--------------------------------------------------------
+# Translates complement of nucleotides: A=T, T=A, C=G, and G=C
+# @param string of text
+# @return string text with complement translation
+# ----------------------------------------------------------------------
 def _complement(nucleotide):
+# Initializes string to be returned for translated complements
     complement = '' 
+# Translates a dna nucleotide to its dna complement nucleotide
     if nucleotide is 'A':    
         complement += 'T'  
     elif nucleotide is 'T':  
@@ -50,10 +63,9 @@ def _complement(nucleotide):
 # Returns output       
     return complement
 #-----------------------------------------------------------------------
-
 # ---MAINCODE-----------------------------------------------------------
 dna = sys.stdin.read().splitlines()
 # If genome/text file is in FASTA format, delete first line of dna
-del dna[0]
+#del dna[0]
 print (reverse_complement(dna))  
 #-----------------------------------------------------------------------
